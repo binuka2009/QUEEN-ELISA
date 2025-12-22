3c8b2a805235b90c6a8c717e0b6a28f3e3417244
binuka2009 <binukaudayanga3@gmail.com>
2025-12-22 11:42:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID",
+SESSION_ID: process.env.SESSION_ID || "QUEEN-ELISA~WNUl0RIY#eBUTEdqFyVi-TtC4K35BW-OW4vVZ28QBwmmY3a5lf7Q",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
